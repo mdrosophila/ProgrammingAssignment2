63346b6ef7d36ab4ad7ad3d1f936cf113e56e60f
mdrosophila <m_drosophila@yahoo.com>
2014-06-12 20:06:54 -0500

    update

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,26 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## makeCacheMatrix function creates 4 functions. These 4 functions are 
-##retrun in a list. The 4 functions are set() get(),setmatrix(x),and getmatrix().
+## makeCacheMatrix() function creates 4 functions. These 4 functions are 
+## retrun in a list. The 4 functions are set() get(),setmatrix(x),and getmatrix().
 ## <<- sign is used to create a global variables m, to store cached 
-##inverse matrix.
-## Example: to solve the reverse of the matrix x {{1,3},{2,4}}
-##>y<- makeCacheMatrix(x)  to cache unsolved x matrix( m=NULL)
-##>cacheSolve(y) to solve the matrix x. The reverse is stored in m
-##>when we try to solve x again calling cacheSolve(y) again, cached data 
-##is returned. No caculcation will be doned.
+## inverse matrix.
+
+## cacheSolve() function returns the inverse of the matrix x. But before it
+## caculates its reverse, it calls the makeCacheMatrix function to see
+## whether the reverse matrix has already been caculated. If the reverse 
+## matrix is cached judding (is.null(m)), the cached data m 
+## is used without calculation. Otherwise, proceed to compute matrix reverse 
+## value and cache the result in m, by calling tmp$setmatrix(m)
+
+## "Usage example": to solve the reverse of the matrix x {{1,3},{2,4}}
+## y<- makeCacheMatrix(x)  to return a function list 
+## It also initiates m with unsolved x matrix( m=NULL)
+## To solve x, call cacheSolve(y) to solve the matrix x. 
+## The solved reverse matrix is stored in m
+## When we try to solve x again calling cacheSolve(y), cached data 
+## is returned. No caculcation will be doned.
 ## To calculate a NEW matrix for example z {{3,5},{4,6}}
-##>y$set(z) set the new matrix to z
-##cacheSolve(y) to calculate its reverse
+## y$set(z) set the new matrix to z
+## cacheSolve(y) to calculate its "z" reverse.
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -32,12 +40,7 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## cacheSolve function returns the inverse of the matrix x. But before it
-## caculates its reverse, it calls the makeCacheMatrix function to see
-##whether the reverse matrix has already been caculated. If the reverse 
-##matrix is cached by makeCacheMatrix fucntion (is.null(m)), the cached data m 
-##is return. Otherwise, proceed to compute matrix reverse value and to 
-##cache the result in m, by calling tmp$setmatrix(m)
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
