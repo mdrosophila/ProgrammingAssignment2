8507e71e87115a85a0998f02a774608a0915faca
mdrosophila <m_drosophila@yahoo.com>
2014-06-16 22:54:54 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,21 +1,21 @@
 
 ## makeCacheMatrix() function creates 4 functions. These 4 functions are 
-## retrun in a list. The 4 functions are set() get(),setmatrix(x),and getmatrix().
-## <<- sign is used to create a global variables m, to store cached 
+## retruned in a list. The 4 functions are set() get(),setmatrix(x),and getmatrix().
+## <<- sign is used to create a global variable m, to store cached 
 ## inverse matrix.
 
 ## cacheSolve() function returns the inverse of the matrix x. But before it
 ## caculates its reverse, it calls the makeCacheMatrix function to see
 ## whether the reverse matrix has already been caculated. If the reverse 
-## matrix is cached judding (is.null(m)), the cached data m 
+## matrix is cached by judding (is.null(m)) statment, the cached data m 
 ## is used without calculation. Otherwise, proceed to compute matrix reverse 
-## value and cache the result in m, by calling tmp$setmatrix(m)
+## value by calling solve() function and cache the result in m, by calling tmp$setmatrix(m)
 
-## "Usage example": to solve the reverse of the matrix x {{1,3},{2,4}}
-## y<- makeCacheMatrix(x)  to return a function list 
+## "Usage example": to solve the reverse of the matrix x {{1,3},{2,4}},
+## y<- makeCacheMatrix(x)  to return a function list. 
 ## It also initiates m with unsolved x matrix( m=NULL)
 ## To solve x, call cacheSolve(y) to solve the matrix x. 
-## The solved reverse matrix is stored in m
+## The solved reverse matrix is stored in m.
 ## When we try to solve x again calling cacheSolve(y), cached data 
 ## is returned. No caculcation will be doned.
 ## To calculate a NEW matrix for example z {{3,5},{4,6}}
